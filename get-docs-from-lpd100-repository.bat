657ba07a65716bcf14b52b061b0b3bf37e244c7f
Caio Gevegir <caiogevegir@hotmail.com>
2020-05-24 16:19:12 -0300

    Created ADK and imported img folders

--- get-docs-from-lpd100-repository.bat
+++ get-docs-from-lpd100-repository.bat
@@ -1,2 +1,2 @@
-cd ..
-copy lpd100\lpd100-corelib.html %~dp0\lpd100-corelib.html
+copy %RPG_PATH%\lpd100-corelib.html %~dp0\lpd100-corelib.html
+copy %RPG_PATH%\lpd100-adk.html %~dp0\lpd100-adk.html
