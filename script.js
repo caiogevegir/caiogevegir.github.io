a6a8404230eca0e15d6607c34996a830a4097093
Caio Gevegir <56521026+caiogevegir@users.noreply.github.com>
2025-04-13 14:44:13 -0300

    Responsive + Script

---
+++ script.js
@@ -0,0 +1,11 @@
+function toogleParagraph(id, button) {
+  const paragraph = document.getElementById(id);
+
+  if (paragraph.style.display === "none" || paragraph.style.display === "") {
+    paragraph.style.display = "block";
+    button.textContent = "Esconder Parágrafo com Spoiler";
+  } else {
+    paragraph.style.display = "none";
+    button.textContent = "Mostrar Parágrafo com Spoiler";
+  }
+}
